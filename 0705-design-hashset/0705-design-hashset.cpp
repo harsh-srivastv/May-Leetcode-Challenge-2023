76af47a073e29cf9334e853bbe45f34254ebc5a1
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2023-05-30 16:52:50 +0530

    Time: 98 ms (66.27%), Space: 43.6 MB (44.20%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+ public:
+  /** Initialize your data structure here. */
+  MyHashSet() : set(1000001) {}
+
+  void add(int key) {
+    set[key] = true;
+  }
+
+  void remove(int key) {
+    set[key] = false;
+  }
+
+  /** Returns true if this set contains the specified element */
+  bool contains(int key) {
+    return set[key];
+  }
+
+ private:
+  vector<bool> set;
+};
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
